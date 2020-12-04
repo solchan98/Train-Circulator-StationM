508f0b22ba2602b7d379f1aeca6b5cfcc6b7aeca
Parksolchan <qkrthfcks1@naver.com>
2020-12-05 01:35:30 +0900

    파일 정리

---
+++ src/DBManager.java
@@ -0,0 +1,17 @@
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+
+public class DBManager {
+	public DBManager() {
+		
+	}
+	public Connection makeDB() throws ClassNotFoundException, SQLException {
+		String url = "jdbc:mysql://www.solac.shop:3306";			
+		Class.forName("com.mysql.jdbc.Driver");
+		System.out.println("DB 연결중 ...");
+		Connection con = DriverManager.getConnection(url, "qkrthfcks", "psc487900!");
+		System.out.println("DB 연결완료^^");
+		return con;
+	}
+}
